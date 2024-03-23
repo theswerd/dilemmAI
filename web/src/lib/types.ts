d5d633140b1ea9918c6325b4a2ba72ba37f725c6
BumbleIV <linganda@msu.edu>
2024-03-23 14:01:35 -0500

    Add types essential for Tournament

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -1,17 +1,47 @@
-
-
-//TODO: discuss this a bit
+// The human user 
 export interface Player {
+  playerID: string;
   name: string;
-  score: number;
-  avatar: string;
+  pfpEmoji: string;
 }
 
-//TODO: discuss this a bit
-export interface Game {
-  isConnected: boolean;
-  players: Player[];
-  currentRound: number;
-  roundScores: Array<[number, number]>;
-  leaderboard: Array<[string, number]>;
+export interface Agent {
+  // Agent Profile
+  agentID: string;
+  name: string;
+  pfpEmoji: string;
+
+  // 
+  playerID: string; // Owner of given agent
+  inputStrategy: string; // Strategy that player/owner inputs for their agent  
 }
+
+// With 8 Agents in a tournament, each agent will play 4 OnevOnes 
+export interface Tournament {
+  tournamentID: string;
+  agents: Agent[];
+  oneVones: OnevOne[];
+  startTime: Date;
+}
+
+// Each OnevOne will have 7 Interactions to cooperate or defect
+export interface OnevOne {
+  onevOneID: string;
+  agents: [Agent, Agent];
+  interactions: Interaction[];
+  winner: Agent | null;
+  startTime: Date;
+}
+
+// Each Interaction will encompass 2 decisions, one from each Agent
+export interface Interaction {
+  interactionID: string;
+  onevOneID: string;
+  decisions: { agentID: string; decision: 'cooperate' | 'defect' }[];
+}
+
+// TODO: Data type for the progress of a given OnevOne
+// 
+
+
+// TODO: Leaderboard data type
