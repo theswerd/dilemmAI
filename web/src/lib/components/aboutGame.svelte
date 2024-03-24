b82abe23378a24f3da7978d372bc9de75d2e4379
Frank Wang <1454884738@qq.com>
2024-03-24 08:26:10 -0500

    chore: update flow chart

--- web/src/lib/components/aboutGame.svelte
+++ web/src/lib/components/aboutGame.svelte
@@ -1,20 +1,29 @@
-<script>
+<script lang="ts">
 	import { base } from '$app/paths';
+	import Mermaid from './mermaid.svelte';
+	const chartDefinition: string = `
+	flowchart TD
+    C(Players make a choice: cooperate or defect)
+    C -->|Both cooperate| D[Both earn 2 coins]
+    C -->|One defects, one cooperates| T[Defector earns 3 coins, cooperator loses 1 coin]
+    C -->|Both defect| W[Neither earns coins]
+    D --> P
+    T --> P
+    W --> P
+    P[Both players reassess]
+    `;
 </script>
 
-<div class="flex h-[657px] w-full items-center justify-center space-x-5 p-[60px]">
+<div class="flex h-[657px] w-full items-center justify-center space-x-5">
 	<div class="max-w-[300px]">
 		<h2 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
 			About the Game
 		</h2>
 		<span>Play and learn the significance of cooperation in the Prisoner’s Dilemma</span>
 	</div>
-	<div>
-		<img
-			src={`${base}/Prisoner's Dilemma-2024-03-24-114502.svg`}
-			alt="GameRule"
-			width={400}
-			height={200}
-		/>
-	</div>
+	<Mermaid chart={chartDefinition} />
 </div>
+
+<style>
+	/* You can also define the height and other styles here if you prefer */
+</style>
