edb3487e2c141b58dacb6ff37d3ee4adc1746afb
Frank Wang <1454884738@qq.com>
2024-03-24 06:51:35 -0500

    chore: fix little graph

--- web/src/lib/components/aboutGame.svelte
+++ web/src/lib/components/aboutGame.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex h-[657px] w-full items-center justify-center space-x-5 p-[60px]">
-	<div>
+	<div class="max-w-[300px]">
 		<h2 class="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
 			About the Game
 		</h2>
@@ -11,10 +11,10 @@
 	</div>
 	<div>
 		<img
-			src={`${base}/Prisoner's Dilemma-2024-03-23-190553.svg`}
+			src={`${base}/Prisoner's Dilemma-2024-03-24-114502.svg`}
 			alt="GameRule"
-			width={300}
-			height={100}
+			width={400}
+			height={200}
 		/>
 	</div>
 </div>
