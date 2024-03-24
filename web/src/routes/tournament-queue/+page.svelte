3a853c5a72f769d831ac83b63e7a70eb8a22e2f0
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-03-24 01:49:13 -0500

    ooof baboof

--- web/src/routes/tournament-queue/+page.svelte
+++ web/src/routes/tournament-queue/+page.svelte
@@ -1,36 +1,37 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import SpinnerUi from '$lib/components/spinner_ui.svelte';
-import { socketStore } from '$lib/stores/socketState';
+	import { socketStore } from '$lib/stores/socketState';
 	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
-    export let data: PageData;
+	export let data: PageData;
 	onMount(async () => {
-        // get agent_id
-        const urlParams = new URLSearchParams(window.location.search);
-        const agent_id = urlParams.get('agent_id');
-        if (!agent_id) {
-            console.error('No agent_id provided');
-            goto('/create-agent');
-        }
-        // sleep 2 seconds
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+		// get agent_id
+		const urlParams = new URLSearchParams(window.location.search);
+		const agent_id = urlParams.get('agent_id');
+		if (!agent_id) {
+			console.error('No agent_id provided');
+			goto('/create-agent');
+		}
+		// sleep 2 seconds
+		await new Promise((resolve) => setTimeout(resolve, 2000));
 		await socketStore.connect();
 	});
 </script>
 
 {#if $socketStore.isConnected}
-	<SpinnerUi 
-        center_item={{ emoji: data.agent.agentEmoji, color: data.agent.agentColor }}
-        outside_items={[
-            { emoji: undefined, color: '#bbbbbb' , gscale: true},
-            { emoji: undefined, color: '#bbbbbb', gscale: true },
-            { emoji: undefined, color: '#bbbbbb', gscale: true },
-        ]}
-    
-    />
+	<SpinnerUi
+		center_item={{ emoji: data.agent.agentEmoji, color: data.agent.agentColor }}
+		outside_items={[
+			{ emoji: undefined, color: '#bbbbbb', gscale: true },
+			{ emoji: undefined, color: '#bbbbbb', gscale: true },
+			{ emoji: undefined, color: '#bbbbbb', gscale: true }
+		]}
+	/>
 {:else}
-	<div class="transition-ease relative flex h-screen w-screen items-center justify-center text-center">
+	<div
+		class="transition-ease relative flex h-screen w-screen items-center justify-center text-center"
+	>
 		{#each new Array(10) as _, i}
 			<div
 				class="condenser absolute left-1/2 top-1/2 aspect-square -translate-x-1/2 -translate-y-1/2 rounded-full border-[1px] border-gray-200 bg-white shadow-md"
